076033a64b3d72944926f7d8ad3dad02975d2294
Terrible <tomasko787@gmail.com>
2023-11-22 12:30:22 +0200

    add math functions

---
+++ script.js
@@ -0,0 +1,41 @@
+function add(num1, num2) {
+return num1 + num2;
+}
+
+function subtract(num1, num2) {
+return num1 - num2;
+}
+
+function divide(num1, num2) {
+return num1 / num2;
+}
+
+function multiply(num1, num2) {
+return num1 * num2;
+}
+
+function operate(num1, num2, operator) {
+switch(operator) {
+    case '+':
+        return add(num1,num2);
+
+    case '-':
+        return subtract(num1,num2);;
+
+    case 'x':
+        return multiply(num1,num2);;
+
+    case '÷':
+        return divide(num1,num2);;
+
+    default:
+        console.log('error');
+        break;
+
+}
+}
+
+let num1;
+let num2;
+let operator;
+
